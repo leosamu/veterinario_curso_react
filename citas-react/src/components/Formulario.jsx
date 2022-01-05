b4f50c1161f1f57b27be5361ac7043881199830c
lsalom <lsalom@mindded.com>
2022-01-05 08:48:36 +0100

    onchange del hook f65

--- citas-react/src/components/Formulario.jsx
+++ citas-react/src/components/Formulario.jsx
@@ -1,6 +1,12 @@
 import { useState, useEffect } from "react";
 
 const Formulario = () => {
+    //MEMO - los hooks deben colorcarse en la parte superior del componente que los van a usar
+    //no se pueden declarar dentro de condicionales
+    const [nombre, setNombre] = useState('');
+
+
+
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -12,7 +18,10 @@ const Formulario = () => {
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
                     </label>
-                    <input id="mascota" type="text" placeholder="Nombre de la Mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                    <input id="mascota" type="text" placeholder="Nombre de la Mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                    />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
