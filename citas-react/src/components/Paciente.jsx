bddc2818e1d2d00fe2f21149ae15de3ce7bf235d
lsalom <lsalom@mindded.com>
2022-01-05 08:23:30 +0100

    diseño listadopacientes f61

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,22 @@
+const Paciente = () => {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl '>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: {''}
+                <span className='font-normal normal-case'>Leonardo</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Email: {''}
+                <span className='font-normal normal-case'>correo@correo.com</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alata: {''}
+                <span className='font-normal normal-case'>10 de diciembre 2002</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: {''}
+                <span className='font-normal normal-case'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, maiores pariatur dolore doloribus quae non omnis iusto nam a modi sit earum quo perspiciatis in magnam, aperiam doloremque laboriosam incidunt.</span>
+            </p>
+        </div>
+    )
+}
+export default Paciente
