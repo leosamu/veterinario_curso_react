ac535ecc47f7a3a2751f8125651d97b74fca60af
lsalom <lsalom@mindded.com>
2022-01-05 10:43:55 +0100

    pasamos los datos al listado pacientes f75

--- citas-react/src/components/Paciente.jsx
+++ citas-react/src/components/Paciente.jsx
@@ -1,20 +1,21 @@
-const Paciente = () => {
+const Paciente = ({ paciente }) => {
+    const { nombre, propietario, email, fecha, sintomas } = paciente;
     return (
         <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl '>
             <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: {''}
-                <span className='font-normal normal-case'>Hook</span>
+                <span className='font-normal normal-case'>{nombre}</span>
             </p>
             <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: {''}
-                <span className='font-normal normal-case'>Leonardo</span>
+                <span className='font-normal normal-case'>{propietario}</span>
             </p>
             <p className='font-bold mb-3 text-gray-700 uppercase'>Email: {''}
-                <span className='font-normal normal-case'>correo@correo.com</span>
+                <span className='font-normal normal-case'>{email}</span>
             </p>
-            <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alata: {''}
-                <span className='font-normal normal-case'>10 de diciembre 2002</span>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta: {''}
+                <span className='font-normal normal-case'>{fecha}</span>
             </p>
             <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: {''}
-                <span className='font-normal normal-case'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate, maiores pariatur dolore doloribus quae non omnis iusto nam a modi sit earum quo perspiciatis in magnam, aperiam doloremque laboriosam incidunt.</span>
+                <span className='font-normal normal-case'>{sintomas}</span>
             </p>
         </div>
     )
